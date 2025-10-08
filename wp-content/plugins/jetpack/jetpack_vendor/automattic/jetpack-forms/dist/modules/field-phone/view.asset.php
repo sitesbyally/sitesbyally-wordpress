9fdeba5df1029422578c59af859995ece63dab37
Ally Anderson <ally@sitesbyally.ca>
2025-10-08 16:46:57 -0600

    remove unnecessary tracked files

--- wp-content/plugins/jetpack/jetpack_vendor/automattic/jetpack-forms/dist/modules/field-phone/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('@wordpress/interactivity', 'wp-polyfill'), 'version' => '3c96fac470ceaae3a883', 'type' => 'module');
--- wp-content/plugins/jetpack/jetpack_vendor/automattic/jetpack-forms/dist/modules/form-step-navigation/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('@wordpress/interactivity', 'wp-polyfill'), 'version' => '202fdad37cfe23484d87', 'type' => 'module');
