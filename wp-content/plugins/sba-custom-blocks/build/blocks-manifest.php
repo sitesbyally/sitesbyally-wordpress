48e0912d299494c1fb44cce00dd68b76529a96a7
Ally Anderson <ally@sitesbyally.ca>
2026-01-18 16:36:31 -0700

    Added scaffolding for new custom blocks. Added theme.json for theme colour palette and typography (in progress).

---
+++ wp-content/plugins/sba-custom-blocks/build/blocks-manifest.php
@@ -0,0 +1,26 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'sba-custom-blocks' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'create-block/sba-custom-blocks',
+		'version' => '0.1.0',
+		'title' => 'Sba Custom Blocks',
+		'category' => 'widgets',
+		'icon' => 'smiley',
+		'description' => 'Example block scaffolded with Create Block tool.',
+		'example' => array(
+			
+		),
+		'supports' => array(
+			'html' => false
+		),
+		'textdomain' => 'sba-custom-blocks',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css',
+		'render' => 'file:./render.php',
+		'viewScript' => 'file:./view.js'
+	)
+);
---
+++ wp-content/plugins/sba-custom-blocks/build/sba-custom-blocks/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'eea6e3b678d47a7dee14');
